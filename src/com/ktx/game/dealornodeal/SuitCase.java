7aa5fda0ce4313a0717e05b84301cf90781b5813
Xum <gian0036@umn.edu>
2013-06-23 15:34:29 -0500

    adding SuitCase

--- src/com/ktx/game/dealornodeal/SuitCase.java
+++ src/com/ktx/game/dealornodeal/SuitCase.java
@@ -1,7 +1,18 @@
 package com.ktx.game.dealornodeal;
 
+import android.R.bool;
+
 public class SuitCase {
-
-
+	int id;
+	int value; 
+	bool open; 
+	public int getId() {
+		return id; 
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	 
 
 }
