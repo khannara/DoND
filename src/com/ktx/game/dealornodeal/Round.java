6cb80c8b623ff34ecb71c2832927dcfaef639f62
tharovuth <tharovuth@gmail.com>
2013-06-23 15:39:16 -0500

    Change package name in AndroidManifest.xml

--- src/com/ktx/game/dealornodeal/Round.java
+++ src/com/ktx/game/dealornodeal/Round.java
@@ -1,5 +1,16 @@
 package com.ktx.game.dealornodeal;
 
 public class Round {
-
+	
+	Round(int round)
+	{
+		
+	}
+	
+	public int numCasesToOpen()
+	{
+		return mCasesToOpen;
+	}
+	
+	private int mCasesToOpen;
 }
--- src/com/ktx/game/dealornodeal/RoundKeeper.java
+++ src/com/ktx/game/dealornodeal/RoundKeeper.java
@@ -1,7 +1,14 @@
 package com.ktx.game.dealornodeal;
 
 public class RoundKeeper {
-
-
-
+	
+	RoundKeeper()
+	{
+		
+	}
+	
+	Round getRound()
+	{
+		return new Round();
+	}
 }
