7361d9072e52faff399e2f42e6c3f8a6cbe08bfa
Xum <gian0036@umn.edu>
2013-06-29 13:58:52 -0500

    fix some indentations

--- src/com/ktx/game/dealornodeal/Round.java
+++ src/com/ktx/game/dealornodeal/Round.java
@@ -1,6 +1,12 @@
 package com.ktx.game.dealornodeal;
 
+import java.util.Observable;
+
 public class Round {
+	//TODO: do whatever when get updated;
+		public void update(Observable observable, Object object) {
+
+		}
 	
 	Round(int round)
 	{
--- src/com/ktx/game/dealornodeal/RoundKeeper.java
+++ src/com/ktx/game/dealornodeal/RoundKeeper.java
@@ -1,6 +1,14 @@
 package com.ktx.game.dealornodeal;
 
-public class RoundKeeper {
+import java.util.Observable;
+import java.util.Observer;
+
+
+public class RoundKeeper implements Observer {
+	//TODO: do whatever when get updated;
+	public void update(Observable observable, Object object) {
+
+	}
 	
 	RoundKeeper()
 	{
