c850ec6030ea3825f2c59e221c8ff88a18fc445f
Xum <gian0036@umn.edu>
2013-06-30 14:44:40 -0500

    stupid ass add

--- src/com/ktx/game/dealornodeal/Round.java
+++ src/com/ktx/game/dealornodeal/Round.java
@@ -1,8 +1,9 @@
 package com.ktx.game.dealornodeal;
 
 import java.util.Observable;
+import java.util.Observer;
 
-public class Round {
+public class Round implements Observer{
 	//TODO: do whatever when get updated;
 		public void update(Observable observable, Object object) {
 
--- src/com/ktx/game/dealornodeal/SuitCase.java
+++ src/com/ktx/game/dealornodeal/SuitCase.java
@@ -1,18 +1,101 @@
 package com.ktx.game.dealornodeal;
 
-import android.R.bool;
+import java.util.ArrayList;
+import java.util.Observable;
+import java.util.Observer;
 
-public class SuitCase {
-	int id;
-	int value; 
-	bool open; 
-	public int getId() {
-		return id; 
-	}
+public class SuitCase extends Observable{
+	private ArrayList<Observer> observers = new ArrayList<Observer>();  
+	private final int id;
+	private final int value; 
+	boolean open;
 	
-	public int getValue() {
-		return value;
+	private SuitCase(SuitCaseBuilder suitCaseBuilder) {
+		id 				= suitCaseBuilder.id;
+		value 			= suitCaseBuilder.value;
+		open 			= suitCaseBuilder.open; 
+		observers 		= suitCaseBuilder.observers;
 	}
-	 
+	public static class SuitCaseBuilder {
+		// required fields
+		private final int id;  
+		private final int value; 
+		// optional fields
+		boolean open = false; 
+		private ArrayList<Observer> observers = new ArrayList<Observer>();  
 
+		public SuitCaseBuilder(int id, int value) {
+			this.id = id; 
+			this.value = value; 
+		}	
+		public SuitCaseBuilder open(boolean input) {
+			open = input; 
+			return this; 
+		}
+		public SuitCaseBuilder observers (ArrayList<Observer> input) {
+			observers = input;
+			return this;
+		}
+		public SuitCase build() {
+			return new SuitCase(this);
+		}
+	}
+
+	public void openCase() {  
+        if(this.open != true) {  
+            this.open = true;  
+            setChanged();  
+            notifyObservers(this, true); 
+        }  
+	}
+        
+    public void notifyObservers(Observable observable, boolean open) {  
+        for (Observer ob : observers) {  
+            ob.update(observable,this.open);  
+        }  
+    }      
+
+    public void registerObserver(Observer observer) {  
+        observers.add(observer);   
+    }  
+ 
+    public void removeObserver(Observer observer) {  
+        observers.remove(observer);
+    }
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
