5867ef2841d03f1182964bb208b2a91b572f4cd9
tharovuth <tharovuth@gmail.com>
2013-07-04 14:49:09 -0500

    Hooked the suitcase to the onClick method to allow the suitcase to notify its observers, like the round keeper.

--- src/com/ktx/game/dealornodeal/RoundKeeper.java
+++ src/com/ktx/game/dealornodeal/RoundKeeper.java
@@ -7,7 +7,8 @@ import java.util.Observer;
 public class RoundKeeper implements Observer {
 	//TODO: do whatever when get updated;
 	public void update(Observable observable, Object object) {
-
+		SuitCase suitcase = (SuitCase) object;
+		System.out.println("suitcase: " + suitcase.id() + " value: " + suitcase.value());
 	}
 	
 	RoundKeeper()
